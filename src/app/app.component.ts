79d78730cb75811023a4e7f6ad46e4bae5f25f78
Andrew Eckart <andrew.g.eckart@gmail.com>
2018-10-31 21:52:45 -0400

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AppData} from './models/appData';
 import {AngularFireDatabase} from '@angular/fire/database';
@@ -12,7 +12,7 @@ import {AngularFireFunctions} from '@angular/fire/functions';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   public record = NBA_RECORD_3PM;
   public data$: Observable<AppData>;
 
@@ -21,32 +21,12 @@ export class AppComponent implements OnInit, AfterViewInit {
     private db: AngularFireDatabase,
     private fns: AngularFireFunctions
   ) {
-    const url = 'https://platform.twitter.com/widgets.js';
-    if (!document.querySelector(`script[src='${url}']`)) {
-      const script = document.createElement('script');
-      script.src = url;
-      document.body.appendChild(script);
-    }
   }
 
   ngOnInit(): void {
     this.data$ = this.db.object('/').valueChanges() as Observable<AppData>;
     this.fetchData();
     setInterval(this.fetchData.bind(this), 30000);
-  }
-
-  ngAfterViewInit(): void {
-    const d = document;
-    const s = 'script';
-    const id = 'twitter-wjs';
-    let js: any;
-    const fjs = d.getElementsByTagName(s)[0];
-    if (!d.getElementById(id)) {
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://platform.twitter.com/widgets.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }
   }
 
   fetchData() {
